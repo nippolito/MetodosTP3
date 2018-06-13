e3389b4e6b2245bffb080eb68fd66a95bae613ad
Peter <megalovania@ubuntu>
2018-06-13 00:05:07 -0300

    Arranco la diversion gente

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "Image.cpp"
+#include "TCSimulator.cpp"
+#include <fstream>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string path = "../imgs_TC/tomo.ppm";
+	TCSimulator Simulator(path);
+	vector<vector<int>> matrizDistancias(Simulator.getHeight(), vector<int>(Simulator.getWidth()));
+	int rayTime = Simulator.createTCRay(pair<int,int>(0,5), pair<int,int>(99,5), matrizDistancias);
+	return 1;
+}
